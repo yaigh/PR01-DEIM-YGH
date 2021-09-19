521afe9384d66df22f1adb868d200ec0dab59072
Yaiza <yaizagh2001@gmail.com>
2021-09-19 13:13:49 +0200

    2-2.A
    Bucles con while

--- Practica_01_YG/Assets/Script/Primero.cs
+++ Practica_01_YG/Assets/Script/Primero.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 public class Primero : MonoBehaviour
 {
 
-    public int var1 = 6;
-    public int var2 = 3;
-    public int var3;
+    int ejem1 = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Variables de ambito publico
+        // Bucle while
 
-        int var3 = var1 * var2;
-        print(var3);
+        while (ejem1 < 10)
+        {
+            ejem1++;
+            print(ejem1);
 
+        }
     }
 
     // Update is called once per frame
