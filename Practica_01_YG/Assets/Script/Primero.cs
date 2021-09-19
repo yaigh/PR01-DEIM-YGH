9d935178ff5f166927ff84c7baeb0f91eeff3726
Yaiza <yaizagh2001@gmail.com>
2021-09-19 12:54:53 +0200

    1-1.A

--- Practica_01_YG/Assets/Script/Primero.cs
+++ Practica_01_YG/Assets/Script/Primero.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class Primero : MonoBehaviour
 {
+
+    float men1 = 10f;
+    float men2 = 8f;
+    float men3;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Variable float
+        float men3 = men1 - men2;
+        print(men3);
+
+
+
     }
 
     // Update is called once per frame
