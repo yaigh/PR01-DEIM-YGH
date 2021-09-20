6de576eaaf46de117e31e8d5642ecff6866377e1
yaigh <yaizagh2001@gmail.com>
2021-09-20 17:56:40 +0200

    3-A CLASE
    Ejercicio 3-A que ha explicado en clase como ejercicio de ejemplo.

--- Practica_01_YG/Assets/Script/Primero.cs
+++ Practica_01_YG/Assets/Script/Primero.cs
@@ -2,21 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Primero : MonoBehaviour
 {
+    
 
-    
-    
+
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-
-       
+        Saludar("Yaiza" , 20);
+        Saludar("Adrian", 21);
+        Saludar("Patri", 22);
 
     }
-
+    void Saludar(string nombre, int edad)
+    {
+        print("Hola " + nombre + " Edad " + edad);
+    }
     
 
 // Update is called once per frame
