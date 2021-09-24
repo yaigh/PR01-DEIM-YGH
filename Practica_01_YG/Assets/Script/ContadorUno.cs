0fc5c06ff68c81c1e87b1ee2e1bc24884447a980
Yaiza <yaizagh2001@gmail.com>
2021-09-24 17:51:32 +0200

    3.D
    Crear un reloj en
    formato “HH:MM:SS”

--- Practica_01_YG/Assets/Script/ContadorUno.cs
+++ Practica_01_YG/Assets/Script/ContadorUno.cs
@@ -5,22 +5,47 @@ using UnityEngine.UI;
 
 public class ContadorUno : MonoBehaviour
 {
-    [SerializeField] Text men1;
+    [SerializeField] Text text1;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int n = 0; n <= 30; n++)
-        {
-            men1.text = "Contador: " + n;
-           
-        }
-       
-
+        StartCoroutine("Cronometro");
     }
 
+    IEnumerator Cronometro()
+    {
+        
+        int x = 0;
+        int y = 0;
+        int z = 0;
+        
+        while (true)
+        {
+            
+            print("La hora es" + z + " : " + y + " : " + x + " : ");
+            
+            text1.text = "La hora es " + z.ToString("D2") + " : " + y.ToString("D2") + " : " + x.ToString("D2") + " : ";
+            x++;
+            
+            yield return new WaitForSeconds(1f);
+            
+            if (x==60)
+            {
+                x = 0;
+                y++;
+            }
+            if (y==60)
+            {
+                y = 0;
+                z++;
+            }
+            
+            
+        }
+    }
 
-    
 
     // Update is called once per frame
     void Update()
