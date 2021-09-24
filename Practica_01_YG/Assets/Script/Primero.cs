dbf472075cae52621277ef672d3fd68888db4918
Yaiza <yaizagh2001@gmail.com>
2021-09-24 12:53:17 +0200

    Probando Vector3
    Prueba sin terminar y sin resultados

--- Practica_01_YG/Assets/Script/Primero.cs
+++ Practica_01_YG/Assets/Script/Primero.cs
@@ -5,26 +5,25 @@ using UnityEngine;
 
 public class Primero : MonoBehaviour
 {
-    float numero1 = 0f;
+    private Vector3 Initpos;
+    [SerializeField] Vector3 Destpos;
+    private Vector3 Finalpos;
+    public float speed = 4f;
 
 
     // Start is called before the first frame update
     void Start()
     {
         
-
     }
     
 
 // Update is called once per frame
 void Update()
     {
-        while (numero1 < 20)
-        {
-            numero1++;
-            print(numero1);
-
-        }
+        Initpos = transform.position;
+        Finalpos = Initpos + Destpos * Time.deltaTime * speed;
+        print(Initpos);
 
     }
 }
